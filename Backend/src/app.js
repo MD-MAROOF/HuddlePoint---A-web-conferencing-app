a5dad0b05816aa7192a316e623e5c568e50c7ee2
Maroof MD <sayyedmaroof504@gmail.com>
2026-01-24 12:16:18 -0600

    Initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+import connectToSocket from "./controllers/socketmanager.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+const { MONGO_URI, PORT } = process.env;
+
+app.set("port", PORT || 8000);
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true }));
+
+// app.get("/home", (req,res)=>{
+//     return res.json("hello");
+// });
+
+const start = async() => {
+    if (!MONGO_URI) {
+        throw new Error("MONGO_URI is not set");
+    }
+
+    const connectionDb = await mongoose.connect(MONGO_URI);
+
+    console.log(`MONGO Connected to DB Host: ${connectionDb.connection.host}`);
+    console.log("Connected to DB");
+    server.listen(app.get("port"), ()=>{
+        console.log("Listening on Port 8000");
+    });
+
+
+}
+
+start();
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+
+        username: {
+            type: String, 
+            unique: true
+        },
+
+        password: {
+            type: String, 
+            required: true 
+        },
+
+        token:{
+            type: String
+        }
+    })
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
